b6733c5ea97a018b63ad20ca1766ee7f011bbb9c
HoroOrangeWolf <wojciech.minner@gmail.com>
2023-10-17 20:23:07 +0200

    init

---
+++ core/src/test/java/com/password/manager/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.password.manager.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
